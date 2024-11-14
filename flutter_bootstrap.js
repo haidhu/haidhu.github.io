2e83521d5901100a105ffcd00d566a3ec96dac4a
haidhu <154666230+haidhu@users.noreply.github.com>
2024-11-15 03:17:06 +0800

    official 1.0 ver.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1598220806"
+    serviceWorkerVersion: "1103114788"
   }
 });
